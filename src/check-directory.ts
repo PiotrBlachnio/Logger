1b1427734b0e5922d2f477aef4181399b7f9fe17
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-05-23 22:11:12 +0200

    Change #fields into private ones

--- src/check-directory.ts
+++ src/check-directory.ts
@@ -1,11 +1,13 @@
 import { exists, mkdir } from "fs";
 
-export default (path: string): void | never => {
+export default (path: string, callback: Function): void | never => {
     exists(path, (isDirectory) => {
         if(!isDirectory) {
             mkdir(path, (err) => {
                 if(err) throw err;
             });
         };
+
+        callback();
     });
 };
--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,40 @@ import validate from './validate';
 import checkDirectory from './check-directory';
 
 export default class Logger {
-    #path: string;
-    #filename: string;
+    private path: string;
+    private filename: string;
 
     constructor({ path, filename }: { path: string, filename: string }) {
         validate({ path, filename });
 
-        this.#path = path;
-        this.#filename = filename;
+        this.path = path;
+        this.filename = filename;
     };
 
     public getPath(): string {
-        return this.#path;
+        return this.path;
     };
 
     public setPath(path: string): void {
-        this.#path = path;
+        this.path = path;
     };
 
     public getFilename(): string {
-        return this.#filename;
+        return this.filename;
     };
 
     public setFilename(filename: string) {
-        this.#filename = filename;
+        this.filename = filename;
     };
 
     public log(data: object): void {
-        checkDirectory(this.#path);
+        checkDirectory(this.path, () => {
+            const fullPath: string = `${this.path}/${this.filename}`;
+            const fileInput: string = `\n${JSON.stringify({ ...data, time: Date.now() })}`;
 
-        appendFile(`${this.#path}/${this.#filename}`, `${JSON.stringify(data)}`, (err) => {
-            if (err) throw err;
+            appendFile(fullPath, fileInput, (err) => {
+                if(err) throw err;
+            });
         });
     };
 };
