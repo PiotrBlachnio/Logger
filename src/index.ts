6d7de00740bd5c01c867d612783a972460aa1baa
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-05-23 19:14:23 +0200

    Create log function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-import { appendFile } from 'fs';
+import { appendFile, mkdir, exists } from 'fs';
 
 export default class Logger {
     private path: string;
+    private filename: string;
 
-    constructor({ path }: { path: string }) {
+    constructor({ path, filename }: { path: string, filename: string }) {
         this.path = path;
+        this.filename = filename;
     };
 
-    public log(type: string, message: string): void {
-        
+    public log(data: object): void {
+        exists(this.path, (isDir: boolean) => {
+            if(!isDir) {
+                mkdir(this.path, (err) => {
+                    if(err) throw err;
+
+                });
+            };
+        });
+
+        appendFile(`${this.path}/${this.filename}`, `${JSON.stringify(data)}`, (err) => {
+            if (err) throw err;
+        });    
     };
 };
