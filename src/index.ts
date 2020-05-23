ceee63a49558edcae9398c78389e16734bb6b106
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-05-23 21:18:27 +0200

    Create getters and setters

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
 import { appendFile, mkdir, exists } from 'fs';
 
 export default class Logger {
-    private path: string;
-    private filename: string;
+    #path: string;
+    #filename: string;
 
     constructor({ path, filename }: { path: string, filename: string }) {
-        this.path = path;
-        this.filename = filename;
+        this.#path = path;
+        this.#filename = filename;
+    };
+
+    public getPath(): string {
+        return this.#path;
+    };
+
+    public setPath(path: string): void {
+        this.#path = path;
+    };
+
+    public getFilename(): string {
+        return this.#filename;
+    };
+
+    public setFilename(filename: string) {
+        this.#filename = filename;
     };
 
     public log(data: object): void {
-        exists(this.path, (isDir: boolean) => {
+        exists(this.#path, (isDir: boolean) => {
             if(!isDir) {
-                mkdir(this.path, (err) => {
+                mkdir(this.#path, (err) => {
                     if(err) throw err;
 
                 });
             };
         });
 
-        appendFile(`${this.path}/${this.filename}`, `${JSON.stringify(data)}`, (err) => {
+        appendFile(`${this.#path}/${this.#filename}`, `${JSON.stringify(data)}`, (err) => {
             if (err) throw err;
-        });    
+        });
     };
 };
