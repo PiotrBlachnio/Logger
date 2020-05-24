64953d9a4fdf57b4e900a14ca516d9fed8f15eb2
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-05-24 21:59:26 +0200

    Add documentation to check directory function

--- src/check-directory.ts
+++ src/check-directory.ts
@@ -1,5 +1,11 @@
 import * as fs from "fs";
 
+/**
+ * Check if directory exists, if does not, then create new one
+ * 
+ * @param {string} path Path to the directory that will be checked
+ * @return {void | never} When directory exists or not, resolve promise
+ */
 export default (path: string): Promise<void | never> => {
     return new Promise(async(resolve, reject) => {
         try {
