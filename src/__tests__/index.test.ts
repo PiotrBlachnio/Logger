5e90b360aa9f7bb31d71ecfb3af15c5808cecca3
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-24 12:32:46 +0200

    Add tests to remove logs method

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -9,21 +9,21 @@ afterAll(() => {
 describe('Logger class', () => {
     const logger: Logger = new Logger({ path: path.join(__dirname, '/testDirectory'), filename: 'test.log' });
 
-    describe('Get path function', () => {
+    describe('Get path method', () => {
         it('Should return correct path', (done) => {
             expect(logger.getPath()).toEqual(path.join(__dirname, '/testDirectory'));
             done();
         });
     });
 
-    describe('Get filename function', () => {
+    describe('Get filename method', () => {
         it('Should return correct filename', (done) => {
             expect(logger.getFilename()).toEqual('test.log');
             done();
         });
     });
 
-    describe('Set path function', () => {
+    describe('Set path method', () => {
         it('Should set path successfully', (done) => {
             logger.setPath(path.join(__dirname, '/testDirectory2'));
 
@@ -32,7 +32,7 @@ describe('Logger class', () => {
         });
     });
 
-    describe('Set filename function', () => {
+    describe('Set filename method', () => {
         it('Should set filename successfully', (done) => {
             logger.setFilename('test2.log');
 
@@ -41,7 +41,7 @@ describe('Logger class', () => {
         });
     });
 
-    describe('Log function', () => {
+    describe('Log method', () => {
         it('Should append log to file successfully', async(done) => {
             const data: { message: string } = { message: 'Hello world!' };
             await logger.log(data);
@@ -55,11 +55,21 @@ describe('Logger class', () => {
         });
     });
 
-    describe('Find logs function', () => {
+    describe('Find logs method', () => {
         it('Should filter and return logs successfully', async(done) => {
             const logs: object[] = await logger.findLogs({ message: 'Hello world!' });
 
             expect(logs[0]).toMatchObject({ message: 'Hello world!' });
+            done();
+        });
+    });
+
+    describe('Remove logs method', () => {
+        it('Should successfully remove logs from the file', async (done) => {
+            await logger.removeLogs();
+            const logs: object[] = await logger.findLogs({});
+
+            expect(logs).toHaveLength(0);
             done();
         });
     });
